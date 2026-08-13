214b15a9cb39c9df84ba4a7499028c6d53cbfefb
dajiaohuang <mikewushuwen@outlook.com>
2026-08-13 23:34:24 +0800

    fix: normalize migration imports

--- migrations/versions/c5f887f6f8ac_add_module_studio_m1_m4.py
+++ migrations/versions/c5f887f6f8ac_add_module_studio_m1_m4.py
@@ -7,9 +7,8 @@ Create Date: 2026-08-13 04:29:00.430092
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 revision: str = "c5f887f6f8ac"
 down_revision: Union[str, None] = "2d87c3d3ec08"
