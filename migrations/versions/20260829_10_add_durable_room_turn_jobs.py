45dea2f2574386f858458a1e150f039b5a0cd427
dajiaohuang <mikewushuwen@outlook.com>
2026-08-29 02:39:05 +0800

    fix(migrations): follow legal acceptance revision

--- migrations/versions/20260829_10_add_durable_room_turn_jobs.py
+++ migrations/versions/20260829_10_add_durable_room_turn_jobs.py
@@ -1,7 +1,7 @@
 """add durable room turn jobs and hosted media artifacts
 
-Revision ID: 20260829_09
-Revises: 20260828_08
+Revision ID: 20260829_10
+Revises: 20260829_09
 Create Date: 2026-08-29
 """
 
@@ -12,8 +12,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "20260829_09"
-down_revision: Union[str, None] = "20260828_08"
+revision: str = "20260829_10"
+down_revision: Union[str, None] = "20260829_09"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
